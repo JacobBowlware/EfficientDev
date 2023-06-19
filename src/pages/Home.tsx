6b0b61ac4de2036601913e2475159168e286dcca
Jacob Bowlware <jacobbowlware@gmail.com>
2023-06-19 17:14:01 -0500

    Implement basic file structure & CSS styling.

---
+++ src/pages/Home.tsx
@@ -0,0 +1,19 @@
+
+const Home = () => {
+    return (
+        <>
+            <div className="web-section-grid web-section-dark home-hero">
+                <div className="web-section-grid__item">
+                    <div className="web-section-header ">
+                        <h1>Home</h1>
+                    </div>
+                </div>
+                <div className="web-section-grid__item">
+
+                </div>
+            </div>
+        </>
+    );
+}
+
+export default Home;
