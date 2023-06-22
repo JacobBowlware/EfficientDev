8743ad520b4d68b2f27a8987ea3eee1c9f1af53a
Jacob Bowlware <jacobbowlware@gmail.com>
2023-06-22 17:25:56 -0500

    Implement Resource Recommendations section.

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -3,6 +3,7 @@ import { Link } from 'react-router-dom';
 // Assets
 import manSitting from '../assets/man-sitting.svg';
 import EfficientDevLogo from '../assets/EfficientDevLogo.svg';
+import CodeWithMoshSS from '../assets/CodeWIthMoshSS.png';
 
 // Components
 import PostCard from '../components/PostCard';
@@ -94,6 +95,65 @@ const Home = () => {
                                 )
                             })
                         }
+                    </div>
+                </div>
+            </div>
+            <div className="web-section-cont">
+                <div className="web-section">
+                    <div className="web-section__item">
+                        <h2 className="web-section-header home-posts-header">
+                            Resource Recommendations
+                        </h2>
+                    </div>
+                    <div className="home-resources-container">
+                        <div className="home-resources-container__item">
+                            <h3 className="home-resources__item-header">
+                                Programming
+                            </h3>
+                            <p className="home-resources__item-body">
+                                When it comes to programming, there are a lot of ways to learn new technologies. My
+                                favorite way is through video tutorials.
+                                I find that I learn best when I can see the code in action and follow along.
+                            </p>
+                            <p className="home-resources__item-body">
+                                In my experience, the best place to find high quality, detailed, and easy to follow
+                                tutorials is on <a href="https://codewithmosh.com//" target="_blank" rel="noreferrer">CodeWithMosh.com</a>.
+                                Mosh Hamedani is a software engineer with over 20 years of experience and has taught over 3 million students.
+                                He has a wide variety of courses on his website, ranging from beginner to advanced. I highly recommend
+                                checking out his courses if you are looking to learn a new technology.
+                            </p>
+                        </div>
+                        <div className="home-resources-container__item">
+                            <h3 className="home-resources__item-header">
+                                Productivity
+                            </h3>
+                            <p className="home-resources__item-body">
+                                Productivity is a very important skill to have as a developer. It can be the difference
+                                between finishing a project on time or missing a deadline. There are many ways to improve
+                                your productivity, but one of the best ways is to optimize your life and work environment.
+                            </p>
+                            <p className="home-resources__item-body">
+                                One of the best resources I have found for improving productivity
+                                is the <a href="https://hubermanlab.com/" target="_blank" rel="noreferrer">HubermanLab.com</a>. Dr. Andrew
+                                Huberman is a neuroscientist and tenured professor in the Department of Neurobiology at the Stanford University School of Medicine.
+                                He has a YouTube channel where he shares his research on neuroscience and how it can be applied to improve
+                                productivity, sleep, and overall health. I highly recommend checking out his channel if you are looking to
+                                improve your productivity.
+                            </p>
+                        </div>
+                        <div className="home-resources-container__item">
+                            <h3 className="home-resources__item-header">
+                                Overview
+                            </h3>
+                            <p className="home-resources__item-body">
+                                These are just a few of the resources I have found to be helpful in my journey as a developer.
+                                I encourage you to check them out and see if they can help you as well. I also encourage you to
+                                do some research and find other resources that can help you become a better developer.
+                            </p>
+                            <p className="home-resources__item-body">
+                                If you have any questions or comments, feel free to reach out to me on <a href="https://www.linkedin.com/in/jacob-bowlware-730824246/" target="_blank" rel="noreferrer">LinkedIn</a>.
+                            </p>
+                        </div>
                     </div>
                 </div>
             </div>
